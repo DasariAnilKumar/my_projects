910c6aac3ca633db97de30daf851d4d49824e672
anilkumar <anilkumard707@gmail.com>
2024-04-06 19:06:27 +0530

    committing basic implementation in  files

---
+++ FirstApp/script.js
@@ -0,0 +1,7 @@
+let heading = document.querySelector("h1");
+heading.style.fontSize = "70px";
+let para = document.querySelector("p");
+
+document.addEventListener("click",function(){
+    para.classList.toggle("dis-none");
+})
